f4b0a081e9fd5de802dd19162b5283c333e7263a
Amurkin <stufly29@gmail.com>
2025-10-20 23:43:11 +0300

    -add LessonsMaterials 8_00min

--- Client/Shooter/Assets/Scripts/PlayerCharacter.cs
+++ Client/Shooter/Assets/Scripts/PlayerCharacter.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerCharacter : MonoBehaviour
 {
+    [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private float _speed = 2f;
     private float _inputH;
     private float _inputV;
-
+    private void Start() {
+        _rigidbody=GetComponent<Rigidbody>();
+    }
     public void SetInput(float h, float v) {
         _inputH = h;
         _inputV = v;
@@ -14,12 +17,16 @@ public class PlayerCharacter : MonoBehaviour
         Move();
     }
     private void Move() {
-        Vector3 direction = new Vector3(_inputH, 0, _inputV).normalized;
-        transform.position += direction * Time.deltaTime * _speed;
+        //Vector3 direction = new Vector3(_inputH, 0, _inputV).normalized;
+        //transform.position += direction * Time.deltaTime * _speed;
+
+        Vector3 velocity = (transform.forward * _inputV + transform.right * _inputH).normalized * _speed;
+        _rigidbody.linearVelocity = velocity;
     }
 
-    public void GetMoveInfo(out Vector3 position) {
+    public void GetMoveInfo(out Vector3 position, out Vector3 velocity) {
         position = transform.position;
+        velocity = _rigidbody.linearVelocity;
     }
 
 }
